b3a9fd66c040c4359dc8bd75ed5dfca118078111
Diana Liu <diana.liu@gorillarush.com>
2024-05-22 16:02:48 +1000

    feat: Project initialization

---
+++ schemaTypes/eventTypes.ts
@@ -0,0 +1,11 @@
+import {defineField, defineType} from "sanity"
+
+export const eventType = defineType({
+  name:"event",
+  title:"Event",
+  type:"document",
+  fields:[defineField({
+    name:"name",
+    type:"string",
+  })]
+})
